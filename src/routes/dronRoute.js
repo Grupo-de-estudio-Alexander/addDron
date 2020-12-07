1af97ea47d816bff07739e31ce09f9f74c3a3777
Johnm-sys <Johnm-sys.jhonmenjura12@gmail.com>
2020-12-07 17:34:46 -0500

    union 2.2

--- src/routes/dronRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router()
-const dronControllers = require("../controllers/dronController")
-
-router.route("/").post(dronControllers.createDron)
-router.route("/").get(dronControllers.AllDron)
-
-module.exports = router
