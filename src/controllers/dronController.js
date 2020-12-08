c9a039aeee3b26bcdce3e642cd4b48cb08d2b9e7
ecmartinez <roloo3210@gmail.com>
2020-12-08 10:09:43 -0500

    src

--- src/controllers/dronController.js
+++ src/controllers/dronController.js
@@ -26,6 +26,49 @@ class ControllerDron {
             console.log(error);
         }
     };
+ //API LOGICA DE LA UBICACION DEL DRON 
+    UbicacionDron = async(req,res) => {
+        try{
+            const palabra = req.body.cadena
+            const array = palabra.split("")
+            let ubicacion = [0,0]
+            let estado = 0
+            let tamañoGrilla = 2
+            // 0 = NORTE, 1 ORIENTE, 2 SUR 3 OCCIDENTE
+            array.forEach(letra => {
+                if (letra === "A" ){
+                    if(estado==0) ubicacion[1]+=1;
+                    if(estado==1) ubicacion[0]+=1;
+                    if(estado==2) ubicacion[1]-=1;
+                    if(estado==3) ubicacion[0]-=1;
+                    
+                }else{
+                    if(letra ==='D'){
+                        if(estado === 0)estado=1;
+                        else if(estado==1)estado=2;
+                        else if(estado==2)estado=3;
+                        else if(estado==3)estado=0;
+                }else{
+                    if(letra ==='I'){
+                        if(estado === 0)estado=3;
+                        else if(estado==1)estado=0;
+                        else if(estado==2)estado=1;
+                        else if(estado==3)estado=2;
+                       }
+                    }
+              }
+            });
+            if (ubicacion[0] > tamañoGrilla || ubicacion[0] < 0 ) res.status(400).json("salio de la grilla")
+            if (ubicacion[1] > tamañoGrilla || ubicacion[1] < 0 ) res.status(400).json("salio de la grilla")
+            
+            res.status(200).json(ubicacion)
+
+        }catch(error){
+            console.log(error);   
+        }
+        
+    }
+
 
 }
 
--- src/routes/dronRoute.js
+++ src/routes/dronRoute.js
@@ -1,9 +1,9 @@
 const router = require("express").Router()
 const dronControllers = require("../controllers/dronController")
-const configControllers = require("../controllers/configController")
-
+const configControllers = require("../controllers/configController") 
 router.route("/").post(dronControllers.createDron)
 router.route("/").get(dronControllers.AllDron)
+router.route("/ubicacion").post(dronControllers.UbicacionDron)
 // ruta para cambiar los parametros de la configuración inicial
 router.route("/config").post(configControllers.modificarParametros)
 
